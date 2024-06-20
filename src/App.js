bbce77ac37bd11fdf3fd520633976a0e6afde9af
Thameera Fernando <ThameeraSithnuwan>
2024-06-20 14:56:18 +0530

    update name

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Thameera Sithnuwan</p>
+        <p>Thameera Sithnuwan Fernando</p>
         <a
           className="App-link"
           href="https://reactjs.org"
